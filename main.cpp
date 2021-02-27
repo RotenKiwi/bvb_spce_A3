4dc7c4907226df7f738a7206dc1e2342ddcb1c56
Parth Joshi <71367592+RotenKiwi@users.noreply.github.com>
2021-02-27 12:40:48 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string name;
+int main(){
+    cout<< "enter your name"<<endl;
+cin>> name;
+cout<< "enter password for thr name "<<name<<endl;
+}
