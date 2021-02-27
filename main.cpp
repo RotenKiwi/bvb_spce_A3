7a5772e1f23babad86e7a4bce107c9785e19322a
Parth Joshi <71367592+RotenKiwi@users.noreply.github.com>
2021-02-27 14:14:22 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <string>
 using namespace std;
 string name;
+string pass;
 int main(){
     cout<< "enter your name"<<endl;
 cin>> name;
 cout<< "enter password for thr name "<<name<<endl;
+   cin>>pass;
 }
